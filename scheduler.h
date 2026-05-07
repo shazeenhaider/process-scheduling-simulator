383467e3dc4a13285468ef9d8cc480be8e065ced
shazeenhaider <shazeenhaider00@gmail.com>
2026-05-07 11:57:23 +0530

    Update scheduler.h

--- scheduler.h
+++ scheduler.h
@@ -1,1 +1,22 @@
+#ifndef SCHEDULER_H
+#define SCHEDULER_H
 
+#include <vector>
+
+using namespace std;
+
+struct Process
+{
+    int id;
+    int arrival;
+    int burst;
+    int completion;
+    int waiting;
+    int turnaround;
+};
+
+void fcfs(vector<Process>& processes);
+void sjf(vector<Process>& processes);
+void roundRobin(vector<Process>& processes, int quantum);
+
+#endif
